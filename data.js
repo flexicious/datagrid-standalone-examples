978118b0a679b08f926c36e46d120f31fbd208fc
Flexicious <info@flexicious.com>
2017-05-10 21:43:32 -0400

    added donuts

--- data.js
+++ data.js
@@ -1,5 +1,15 @@
-      var companylist = [{ label: "Company A", state: "NJ", rank: "1.11" },
-      { label: "Company B", state: "PA", rank: "11.1" },
-      { label: "Company C", state: "CT", rank: "-111" },
-      { label: "Company D", state: "NY", rank: "2.34" },
-      { label: "Company E", state: "NJ", rank: "22.2" }];
+var companylist = [{ label: "Company A", state: "NJ", rank: "1.11" },
+{ label: "Company B", state: "PA", rank: "11.1" },
+{ label: "Company C", state: "CT", rank: "-111" },
+{ label: "Company D", state: "NY", rank: "2.34" },
+{ label: "Company E", state: "NJ", rank: "22.2" }];
+
+var donuts = [
+      { "id": "5001", "type": "None" },
+      { "id": "5002", "type": "Glazed" },
+      { "id": "5005", "type": "Sugar" },
+      { "id": "5007", "type": "Powdered Sugar" },
+      { "id": "5006", "type": "Chocolate with Sprinkles" },
+      { "id": "5003", "type": "Chocolate" },
+      { "id": "5004", "type": "Maple" }
+];
